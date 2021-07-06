6c6d2160ad544a7a56056097d163e167278b8741
hashtoes <hashtoes@github.com>
2021-07-06 23:05:09 +0900

    Updates

---
+++ precache-manifest.a7109a6b776842482298205d523c3462.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfa88fe2880b9b1f5ef0efcc51474711",
+    "url": "/shogi-board/index.html"
+  },
+  {
+    "revision": "117043513e6cc1b224a5",
+    "url": "/shogi-board/static/css/main.18744f41.chunk.css"
+  },
+  {
+    "revision": "7ce254c42fb61ce051f1",
+    "url": "/shogi-board/static/js/2.f33bbcbd.chunk.js"
+  },
+  {
+    "revision": "282cad4058e83221804611932e7ca1ed",
+    "url": "/shogi-board/static/js/2.f33bbcbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "117043513e6cc1b224a5",
+    "url": "/shogi-board/static/js/main.a49fedaf.chunk.js"
+  },
+  {
+    "revision": "d33905938a62d0d10e49",
+    "url": "/shogi-board/static/js/runtime-main.fcfd3988.js"
+  }
+]);
